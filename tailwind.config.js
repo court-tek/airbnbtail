9c9968e0a87f2addd4975291ca43b2889da57108
Courtney Girley <courtney@Courtneys-MacBook-Pro-2.local>
2023-08-20 04:55:16 -0400

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        airbRed1: 'hsl(358, 75%, 53%)',
+        airbRed2: 'hsl(333, 94%, 44%)',
+        airbRed3: 'hsl(343, 79%, 51%)',
+        airbRed4: 'hsl(349, 100%, 61%)',
+        airbBlanco: 'hsl(180, 100%, 100%)',
+        airbVerde: 'hsl(121, 100%, 32%)',
+      }
+    },
+  },
+  plugins: [],
+}
+
